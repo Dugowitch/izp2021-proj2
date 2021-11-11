5ac9027ea9fe34267e2681ec9d75842cfff6ca05
Dugowtich <jakubdugovic@gmail.com>
2021-11-11 15:41:11 +0100

    rel, tuple, tuple_print, rel_print

--- setcal.c
+++ setcal.c
@@ -8,9 +8,24 @@ typedef struct {
   size_t size;
 } vec_t;
 
+typedef struct {
+  char *x;
+  char *y;
+} tuple_t;
+
+typedef struct {
+  tuple_t *arr;
+  size_t used;
+  size_t size;
+} rel_t;
+
+/**
+ * @brief BASIC VECTOR FUNCTIONS
+ */
+
 void vec_print(vec_t *v){
-    for(size_t i = 0; i < v->used; i++)
-        printf("%s ", v->arr[i]);
+  for(size_t i = 0; i < v->used; i++)
+      printf("%s ", v->arr[i]);
 }
 
 void vec_init(vec_t *v, size_t size) {
@@ -35,17 +50,38 @@ void vec_free(vec_t *v) {
   v->used = v->size = 0;
 }
 
+/**
+ * @brief BASIC RELATION & TUPLE FUNCTIONS
+ */
+
+void tuple_print(tuple_t t){
+  printf("(%s %s) ",t.x, t.y);
+}
+
+void rel_print(rel_t *r){
+  printf("R ");
+  for(size_t i = 0; i < r->used; i++){
+    tuple_print(r->arr[i]);
+  }
+  printf("\n");
+}
+
 int main(int argc, char *argv[]){
-    (void) argc;
-    (void) argv;
+  (void) argc;
+  (void) argv;
 
-    // vec_t v;
-    // vec_init(&v, 3);
-    // vec_append(&v, "bumbum");
-    // vec_append(&v, "do");
-    // vec_append(&v, "poop poop");
-    // vec_print(&v);
-    // vec_free(&v);
+  // vec_t v;
+  // vec_init(&v, 3);
+  // vec_append(&v, "bumbum");
+  // vec_append(&v, "do");
+  // vec_append(&v, "poop poop");
+  // vec_print(&v);
+  // vec_free(&v);
 
-    return 0;
+  // tuple_t t;
+  // t.x = "abc";
+  // t.y = "def";
+  // tuple_print(t);
+
+  return 0;
 }
